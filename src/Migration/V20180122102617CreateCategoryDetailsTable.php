ffb248e53386ff99c435d5fb007814f8ef6642d9
twinh <twinhuang@qq.com>
2018-01-22 10:27:49 +0800

    增加product相关建表语句 closes #3358

---
+++ src/Migration/V20180122102617CreateCategoryDetailsTable.php
@@ -0,0 +1,30 @@
+<?php
+namespace Miaoxing\Product\Migration;
+
+use Miaoxing\Plugin\BaseMigration;
+
+class V20180122102617CreateCategoryDetailsTable extends BaseMigration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function up()
+    {
+        $this->schema->table('categoryDetails')
+            ->id()
+            ->int('appId')
+            ->string('categoryId', 64)
+            ->longtext('detailUp')
+            ->longtext('detailDown')
+            ->bool('showed')->defaults(true)
+            ->exec();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function down()
+    {
+        $this->schema->dropIfExists('categoryDetails');
+    }
+}
