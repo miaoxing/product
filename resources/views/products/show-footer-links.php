796fe64337b14237ca5e4d7a215247b423a5351f
twinh <twinhuang@qq.com>
2018-05-28 13:50:07 +0800

    商品详情页增加主页入口

--- resources/views/products/show-footer-links.php
+++ resources/views/products/show-footer-links.php
@@ -1,4 +1,10 @@
+<?php if (wei()->setting('product.showIndexLink')) : ?>
+  <a class="product-action-link link-dark border-right" href="<?= $url('index') ?>">
+    <div class="ni ni-home icon"></div>
+  </a>
+<?php endif ?>
 <a class="product-action-link link-dark" href="<?= $url('carts') ?>">
   <span class="js-product-cart-num product-cart-num label label-danger"></span>
   <i class="ni ni-cart"></i>
 </a>
+
