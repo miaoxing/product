d824da35012dce361277128484f014092914b28b
twinh <twinhuang@qq.com>
2018-11-15 13:18:31 +0800

    feat: 产品分享增加首张图片

--- src/Controller/Products.php
+++ src/Controller/Products.php
@@ -180,7 +180,7 @@ class Products extends \Miaoxing\Plugin\BaseController
             ->setHeaderTitle('商品详情')
             ->hideFooter();
         $scoreTitle = $this->setting('score.title', '积分');
-        wei()->share->setTitle($product['name']);
+        wei()->share->setImage($images[0]);
 
         return get_defined_vars();
     }
