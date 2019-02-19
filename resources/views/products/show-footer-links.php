d04616bfd991d2d924e5123a7a5c7401c0404245
twinh <twinhuang@qq.com>
2019-02-19 18:44:43 +0800

    refactoring: link-dark text-dark => text-body

--- resources/views/products/show-footer-links.php
+++ resources/views/products/show-footer-links.php
@@ -1,9 +1,9 @@
 <?php if (wei()->setting('product.showIndexLink')) : ?>
-  <a class="product-action-link link-dark border-right" href="<?= $url('index') ?>">
+  <a class="product-action-link text-body border-right" href="<?= $url('index') ?>">
     <div class="ni ni-home icon"></div>
   </a>
 <?php endif ?>
-<a class="product-action-link link-dark" href="<?= $url('carts') ?>">
+<a class="product-action-link text-body" href="<?= $url('carts') ?>">
   <span class="js-product-cart-num product-cart-num badge badge-danger"></span>
   <i class="ni ni-cart"></i>
 </a>
