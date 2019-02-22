dfdd1aea2678f03553fba51dd5f7b52f6b0d1877
twinh <twinhuang@qq.com>
2019-02-22 12:12:34 +0800

    refactoring: form直接加载unparam

--- resources/views/admin/productCategories/edit.php
+++ resources/views/admin/productCategories/edit.php
@@ -87,7 +87,7 @@
 
 <?= $block->js() ?>
 <script>
-  require(['form', 'ueditor', 'jquery-unparam', 'plugins/admin/js/data-table', 'plugins/app/js/validation'], function (form) {
+  require(['form', 'ueditor', 'plugins/admin/js/data-table', 'plugins/app/js/validation'], function (form) {
     var mallJson = <?= json_encode(wei()->category()->notDeleted()->withParent('mall')->getTreeToArray()) ?>;
     form.toOptions($('#parent-id'), mallJson, 'id', 'name');
     form.toOptions($('.js-list-tpl'), <?= json_encode($wei->product->getListTplsOptions()) ?>, 'name', 'value');
