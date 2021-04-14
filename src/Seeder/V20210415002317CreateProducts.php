421a517da09ed244de34a2bbad82942e3d3f0a7d
twinh <twinhuang@qq.com>
2021-04-15 00:35:17 +0800

    feat: 增加创建页面测试数据的 Seeder

---
+++ src/Seeder/V20210415002317CreateProducts.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Miaoxing\Product\Seeder;
+
+use Miaoxing\Plugin\Seeder\BaseSeeder;
+
+class V20210415002317CreateProducts extends BaseSeeder
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function run()
+    {
+    }
+}
