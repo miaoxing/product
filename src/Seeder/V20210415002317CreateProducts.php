6b4d8be17a8df0a366c03ef1ceaef4abd4a6c608
twinh <twinhuang@qq.com>
2021-05-22 22:20:52 +0800

    feat(product): 增加生成演示数据

--- src/Seeder/V20210415002317CreateProducts.php
+++ src/Seeder/V20210415002317CreateProducts.php
@@ -2,7 +2,11 @@
 
 namespace Miaoxing\Product\Seeder;
 
+use Faker\Factory;
+use Miaoxing\Category\Service\CategoryModel;
 use Miaoxing\Plugin\Seeder\BaseSeeder;
+use Miaoxing\Plugin\Service\Tester;
+use Miaoxing\Product\Service\ProductModel;
 
 class V20210415002317CreateProducts extends BaseSeeder
 {
@@ -11,5 +15,43 @@ class V20210415002317CreateProducts extends BaseSeeder
      */
     public function run()
     {
+        $faker = Factory::create('zh_CN');
+
+        $categoryIds = array_column(CategoryModel::select('id')->fetchAll(), 'id');
+
+        foreach (range(1, 30) as $i) {
+            // TODO 增加 api 方便创建
+            Tester::postAdminApi('products', [
+                'name' => $faker->words(3, true),
+                'images' => [
+                    [
+                        'url' => $faker->imageUrl(640, 640),
+                    ],
+                ],
+                'detail' => [
+                    'content' => $faker->realTextBetween(),
+                ],
+                'categoriesProducts' => [
+                    [
+                        'categoryId' => $faker->randomElement($categoryIds),
+                    ],
+                ],
+                'spec' => [
+                    'specs' => ProductModel::getDefaultSpecs(),
+                ],
+                'skus' => [
+                    [
+                        'price' => $faker->randomFloat(2, 0, 100),
+                        'stockNum' => $faker->biasedNumberBetween(),
+                        'specValues' => [
+                            [
+                                'name' => '默认',
+                                'specName' => '默认',
+                            ],
+                        ],
+                    ],
+                ],
+            ]);
+        }
     }
 }
