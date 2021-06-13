b91511fa4d082c1b91d60b55747abd345f27ff89
twinh <twinhuang@qq.com>
2021-06-13 22:36:19 +0800

    feat(product): 增加获取商品运费模板接口

---
+++ pages/m-api/products/[id]/shipping-tpl/index.php
@@ -0,0 +1,30 @@
+<?php
+
+use Miaoxing\Logistics\Resource\ShippingTplResource;
+use Miaoxing\Plugin\BaseController;
+use Miaoxing\Product\Service\ProductModel;
+
+return new class extends BaseController {
+    public function get($req)
+    {
+        $product = ProductModel::findOrFail($req['id']);
+        if (!$product->isRequireAddress) {
+            return suc(['detail' => '购买该商品无需填写地址']);
+        }
+
+        $shippingTpl = $product->shippingTpl;
+
+        $city = '';
+        if ($req['filterRulesByCity']) {
+            ['city' => $city, 'rules' => $rules] = $shippingTpl->getCityAndRules('深圳市');
+            if ($rules) {
+                $shippingTpl->setRelationValue('rules', $rules);
+            }
+        }
+
+        $shippingTpl->load('rules.service');
+
+        return $shippingTpl->toRet(ShippingTplResource::class)
+            ->with('city', $city);
+    }
+};
