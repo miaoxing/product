61c17d5cdf1073bd5229b88a76465786f4919976
twinh <twinhuang@qq.com>
2021-06-17 18:14:03 +0800

    feat(SkuModel): 增加 `checkCreateCart` 方法，检查当前 SKU 是否可购买

--- src/Service/SkuModel.php
+++ src/Service/SkuModel.php
@@ -6,9 +6,12 @@ use Miaoxing\Plugin\BaseModel;
 use Miaoxing\Plugin\Model\ModelTrait;
 use Miaoxing\Product\Metadata\SkuTrait;
 use Miaoxing\Product\Model\BelongsToProductTrait;
+use Wei\Ret;
 
 /**
  * SKU
+ *
+ * @property array $specValueIds
  */
 class SkuModel extends BaseModel
 {
@@ -35,5 +38,24 @@ class SkuModel extends BaseModel
         return array_merge($this->guarded, [
             'soldNum',
         ]);
+    }
+
+    /**
+     * 检查当前 SKU 是否可购买
+     *
+     * @return Ret
+     */
+    public function checkCreateCart(): Ret
+    {
+        // 加入购物车之后，再删除 SKU 的情况
+        if ($this->isDeleted()) {
+            return err('该商品规格已下架');
+        }
+
+        if ($this->stockNum <= 0) {
+            return err('该商品规格已售罄');
+        }
+
+        return suc('可以购买');
     }
 }
