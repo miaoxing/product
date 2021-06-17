70a38b2a705f91add297db720d0bdbd3c0c2c88c
twinh <twinhuang@qq.com>
2021-06-17 11:12:16 +0800

    feat(SkuModel): 增加商品模型关联

--- src/Service/SkuModel.php
+++ src/Service/SkuModel.php
@@ -5,14 +5,17 @@ namespace Miaoxing\Product\Service;
 use Miaoxing\Plugin\BaseModel;
 use Miaoxing\Plugin\Model\ModelTrait;
 use Miaoxing\Product\Metadata\SkuTrait;
+use Miaoxing\Product\Model\BelongsToProductTrait;
 
 /**
  * SKU
  */
 class SkuModel extends BaseModel
 {
+    use BelongsToProductTrait;
     use ModelTrait;
     use SkuTrait;
+    use SoftDeleteTrait;
 
     protected $attributes = [
         'specValueIds' => [],
