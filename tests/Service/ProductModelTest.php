1c48c1b19cd4d413f697063e5e57ff790697665a
twinh <twinhuang@qq.com>
2021-07-07 22:25:40 +0800

    test(product): 增加方法，用于检查商品是否可以加入购物车或下单

--- tests/Service/ProductModelTest.php
+++ tests/Service/ProductModelTest.php
@@ -4,6 +4,7 @@ namespace MiaoxingTest\Product\Service;
 
 use Miaoxing\Plugin\Test\BaseTestCase;
 use Miaoxing\Product\Service\ProductModel;
+use Wei\Ret;
 
 class ProductModelTest extends BaseTestCase
 {
@@ -56,6 +57,90 @@ class ProductModelTest extends BaseTestCase
                     'stockNum' => 1,
                 ],
                 ProductModel::STATUS_ON_SALE,
+            ],
+        ];
+    }
+
+    /**
+     * @param array $attributes
+     * @param Ret $createCart
+     * @param Ret|null $createOrder
+     * @dataProvider providerForCheckCreateCartOrOrder
+     */
+    public function testCheckCreateCartOrOrder(array $attributes, Ret $createCart, Ret $createOrder = null): void
+    {
+        $product = ProductModel::new($attributes);
+        $ret = $product->checkCreateCartOrOrder();
+
+        if ($createCart->isErr()) {
+            $this->assertRetErr($ret['createCart'], $createCart['message']);
+        } else {
+            $this->assertRetSuc($ret['createCart']);
+        }
+
+        if (!$createOrder) {
+            $createOrder = $createCart;
+        }
+        if ($createOrder->isErr()) {
+            $this->assertRetErr($ret['createOrder'], $createOrder['message']);
+        } else {
+            $this->assertRetSuc($ret['createOrder']);
+        }
+
+        if ($createCart->isErr() && $createOrder->isErr()) {
+            $this->assertRetErr($ret, $createCart['message']);
+        } else {
+            $this->assertRetSuc($ret);
+        }
+    }
+
+    public static function providerForCheckCreateCartOrOrder(): array
+    {
+        return [
+            [
+                [
+                    'status' => ProductModel::STATUS_NOT_STARTED,
+                ],
+                suc(),
+                err('抢购即将开始'),
+            ],
+            [
+                [
+                    'status' => ProductModel::STATUS_ON_SALE,
+                ],
+                suc(),
+            ],
+            [
+                [
+                    'status' => ProductModel::STATUS_ENDED,
+                ],
+                err('抢购结束'),
+            ],
+            [
+                [
+                    'status' => ProductModel::STATUS_SOLD_OUT,
+                ],
+                err('商品已卖光了'),
+            ],
+            [
+                [
+                    'status' => ProductModel::STATUS_DELISTED,
+                ],
+                err('商品已下架'),
+            ],
+            [
+                [
+                    'status' => ProductModel::STATUS_DELETED,
+                ],
+                err('商品已删除'),
+            ],
+            [
+                [
+                    'status' => ProductModel::STATUS_ON_SALE,
+                    'isAllowAddCart' => false,
+                ],
+                err('该商品不可加入购物车'),
+                suc(),
             ],
         ];
     }
