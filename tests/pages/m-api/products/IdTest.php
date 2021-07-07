ab2f0bc7bbfa44ccde90f1da92c2d74d8fa96792
twinh <twinhuang@qq.com>
2021-07-07 22:31:04 +0800

    test(product): 增加获取商品详情接口

---
+++ tests/pages/m-api/products/IdTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace MiaoxingTest\Prodcut\Pages\MApi\Products;
+
+use Miaoxing\Plugin\Service\Tester;
+use Miaoxing\Plugin\Test\BaseTestCase;
+use Miaoxing\Product\Service\ProductModel;
+
+class IdTest extends BaseTestCase
+{
+    public function testGet()
+    {
+        $product = ProductModel::saveAttributes([
+            'name' => '测试商品',
+            'status' => ProductModel::STATUS_ON_SALE,
+        ]);
+
+        $ret = Tester::get('/m-api/products/' . $product->id);
+        $this->assertRetSuc($ret);
+        $this->assertSame('测试商品', $ret['data']['name']);
+
+        $this->assertArrayHasKey('createCartOrOrder', $ret['data']);
+
+        $product->destroy();
+        $this->expectExceptionObject(new \Exception('Record not found', 404));
+        Tester::get('/m-api/products/' . $product->id);
+    }
+
+    public function testGet404()
+    {
+        $this->expectExceptionObject(new \Exception('Record not found', 404));
+
+        Tester::get('/m-api/products/not-found');
+    }
+}
