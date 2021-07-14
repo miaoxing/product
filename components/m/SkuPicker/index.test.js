3c0dbdb3c6558ab1e32870c51fa7b82f1ddf27b6
twinh <twinhuang@qq.com>
2021-07-14 23:58:11 +0800

    test(SkuPicker): 测试关闭

--- components/m/SkuPicker/index.test.js
+++ components/m/SkuPicker/index.test.js
@@ -1,47 +1,54 @@
 import SkuPicker from './index';
-import {render} from '@testing-library/react';
+import {render, fireEvent} from '@testing-library/react';
+import {Ret} from 'miaoxing';
+import {useState} from 'react';
 
-describe('SkuPicker', () => {
-  test('basic', async () => {
-    const {container} = render(<SkuPicker product={{
-      spec: {
-        specs: [
+const product = {
+  spec: {
+    specs: [
+      {
+        id: 3,
+        name: '默认',
+        values: [
           {
-            id: 3,
+            id: 4,
             name: '默认',
-            values: [
-              {
-                id: 4,
-                name: '默认',
-              },
-            ],
           },
         ],
       },
-      skus: [
-        {
-          id: 1,
-          specValueIds: [4],
-          stockNum: 10,
-        },
-      ],
-      configs: {
-        unit: '件',
-      },
-      createCartOrOrder: {
-        code: 0,
-        message: 'ok',
-        createCart: {
-          code: 0,
-          message: 'ok',
-        },
-        createOrder: {
-          code: 0,
-          message: 'ok',
-        },
-      },
-    }}/>);
+    ],
+  },
+  skus: [
+    {
+      id: 1,
+      specValueIds: [4],
+      stockNum: 10,
+    },
+  ],
+  configs: {
+    unit: '件',
+  },
+  createCartOrOrder: Ret.suc({
+    createCart: Ret.suc(),
+    createOrder: Ret.suc(),
+  }),
+};
+
+describe('SkuPicker', () => {
+  test('basic', async () => {
+    const {container} = render(<SkuPicker product={product}/>);
 
     expect(container).toMatchSnapshot();
+  });
+
+  test('close', () => {
+    const handleClose = jest.fn();
+    const {getByText} = render(<SkuPicker isOpened={true} onClose={handleClose} product={product}/>);
+
+    const close = getByText('×');
+    fireEvent.click(close);
+
+    expect(handleClose).toBeCalledTimes(1);
+    expect(handleClose.mock.calls).toMatchSnapshot();
   });
 });
