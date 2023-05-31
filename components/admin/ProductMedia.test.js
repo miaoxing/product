78c9075a21bdb502b0eed52db77625121015d3cf
twinh <twinhuang@qq.com>
2023-05-31 23:02:25 +0800

    test(antd5): 关闭 `hashed`

--- components/admin/ProductMedia.test.js
+++ components/admin/ProductMedia.test.js
@@ -1,12 +1,17 @@
 import ProductMedia from './ProductMedia';
 import {render} from '@testing-library/react';
+import {ConfigProvider} from 'antd';
 
 describe('ProductMedia', () => {
   test('basic', () => {
-    const result = render(<ProductMedia product={{
-      name: 'name',
-      image: 'image',
-    }}/>);
+    const result = render(
+      <ConfigProvider theme={{hashed: false}}>
+        <ProductMedia product={{
+          name: 'name',
+          image: 'image',
+        }}/>
+      </ConfigProvider>
+    );
     expect(result.container).toMatchSnapshot();
   });
 });
