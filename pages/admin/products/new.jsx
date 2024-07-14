c304946804367e4e0c5edc8118796942ab672615
twinh <twinhuang@qq.com>
2024-07-14 15:47:05 +0800

    feat(u): 调整页面内容为卡片视图

--- pages/admin/products/new.jsx
+++ pages/admin/products/new.jsx
@@ -1,16 +1,17 @@
 /**
  * @share [id]/edit
  */
-import {useRef} from 'react';
-import {CListBtn} from '@mxjs/a-clink';
-import {Page, PageActions} from '@mxjs/a-page';
-import {Form, FormItem, FormAction} from '@mxjs/a-form';
-import {Divider, Radio, Switch, AutoComplete} from 'antd';
+import { useRef } from 'react';
+import { CListBtn } from '@mxjs/a-clink';
+import { Page, PageActions } from '@mxjs/a-page';
+import { Form, FormItem, FormAction } from '@mxjs/a-form';
+import { Divider, Radio, Switch, AutoComplete } from 'antd';
 import DateRangePicker from '@mxjs/a-date-range-picker';
 import Sku from '@miaoxing/product/components/admin/Sku';
 import $ from 'miaoxing';
-import {FormUeditor} from '@mxjs/a-ueditor';
-import {FormItemSort, Select, TreeSelect, Upload} from '@miaoxing/admin';
+import { FormUeditor } from '@mxjs/a-ueditor';
+import { FormItemSort, Select, TreeSelect, Upload } from '@miaoxing/admin';
+import { Section } from '@mxjs/a-section';
 
 // TODO 解决 setShippingTpl 后，afterLoad 还未获取到 shippingTpls 模板未选择
 let loadedShippingTpls = [];
@@ -21,10 +22,10 @@ const New = () => {
   const form = useRef();
 
   // 加载运费模板
-  const handleShippingTplsAfterLoad = ({data}) => {
+  const handleShippingTplsAfterLoad = ({ data }) => {
     loadedShippingTpls = data;
     if (!$.req('id') && data.length) {
-      form.current.setFieldsValue({shippingTplId: data[0].id});
+      form.current.setFieldsValue({ shippingTplId: data[0].id });
     }
   };
 
@@ -36,7 +37,7 @@ const New = () => {
 
       <Form
         formRef={form}
-        afterLoad={({ret}) => {
+        afterLoad={({ ret }) => {
           // 将商品分类关联表的 id 缓存起来，以便提交时设置回去
           ret.data.categoryIds = ret.data.categoriesProducts.map(categoryProduct => {
             categoryProductIds.current[categoryProduct.categoryId] = categoryProduct.id;
@@ -45,7 +46,7 @@ const New = () => {
 
           // 图片组件只传入必要的字段，后台的返回的 type 和图片组件的 type 含义不同，传入将出错
           ret.data.images = ret.data.images.map(image => {
-            return {id: image.id, url: image.url};
+            return { id: image.id, url: image.url };
           });
 
           // 如果运费模板先加载完，使用第一个值
@@ -75,93 +76,95 @@ const New = () => {
           return values;
         }}
       >
-        <FormItem label="名称" name="name" required/>
+        <Section>
+          <FormItem label="名称" name="name" required/>
 
-        <FormItem label="简短描述" name="intro"/>
+          <FormItem label="简短描述" name="intro"/>
 
-        <FormItem
-          label="图片"
-          name="images"
-          extra="支持.jpg .jpeg .bmp .gif .png格式照片，最多上传 9 张图片"
-          wrapperCol={{span: 12}}
-        >
-          <Upload max={9}/>
-        </FormItem>
+          <FormItem
+            label="图片"
+            name="images"
+            extra="支持.jpg .jpeg .bmp .gif .png格式照片，最多上传 9 张图片"
+            wrapperCol={{ span: 12 }}
+          >
+            <Upload max={9}/>
+          </FormItem>
 
-        <FormItem label="分类" name="categoryIds">
-          <TreeSelect
-            url="categories"
-            multiple
-            placeholder="请选择"
-          />
-        </FormItem>
+          <FormItem label="分类" name="categoryIds">
+            <TreeSelect
+              url="categories"
+              multiple
+              placeholder="请选择"
+            />
+          </FormItem>
 
-        <Sku ref={skuRef}/>
+          <Sku ref={skuRef}/>
 
-        <FormItem label="运费模板" name="shippingTplId">
-          <Select url="shipping-tpls" afterLoad={handleShippingTplsAfterLoad} labelKey="name" valueKey="id"/>
-        </FormItem>
+          <FormItem label="运费模板" name="shippingTplId">
+            <Select url="shipping-tpls" afterLoad={handleShippingTplsAfterLoad} labelKey="name" valueKey="id"/>
+          </FormItem>
 
-        <FormUeditor label="描述" name={['detail', 'content']}/>
+          <FormUeditor label="描述" name={['detail', 'content']}/>
 
-        <Divider/>
+          <Divider/>
 
-        <FormItem label="是否上架" name="isListing" valuePropName="checked">
-          <Switch/>
-        </FormItem>
+          <FormItem label="是否上架" name="isListing" valuePropName="checked">
+            <Switch/>
+          </FormItem>
 
-        <FormItem label="上架时间" name="_startAt">
-          <DateRangePicker showTime names={['startAt', 'endAt']} format="YYYY-MM-DD HH:mm:00"/>
-        </FormItem>
+          <FormItem label="上架时间" name="_startAt">
+            <DateRangePicker showTime names={['startAt', 'endAt']} format="YYYY-MM-DD HH:mm:00"/>
+          </FormItem>
 
-        <FormItem label="最大购买数量" name="maxOrderQuantity" type="number" extra="0 表示无限制"/>
+          <FormItem label="最大购买数量" name="maxOrderQuantity" type="number" extra="0 表示无限制"/>
 
-        <FormItem label="“数量”名称" name={['configs', 'quantityName']} extra="例如“人数”"/>
+          <FormItem label="“数量”名称" name={['configs', 'quantityName']} extra="例如“人数”"/>
 
-        <FormItem label="单位" name={['configs', 'unit']} extra='默认为"件"'>
-          <AutoComplete
-            options={[
-              {value: '件'},
-              {value: '杯'},
-              {value: '只'},
-              {value: '罐'},
-              {value: '盒'},
-              {value: '人'},
-              {value: '个'},
-            ]}
-          />
-        </FormItem>
+          <FormItem label="单位" name={['configs', 'unit']} extra='默认为"件"'>
+            <AutoComplete
+              options={[
+                { value: '件' },
+                { value: '杯' },
+                { value: '只' },
+                { value: '罐' },
+                { value: '盒' },
+                { value: '人' },
+                { value: '个' },
+              ]}
+            />
+          </FormItem>
 
-        <FormItem label="是否隐藏销量" name={['configs', 'hideSoldNum']} valuePropName="checked">
-          <Switch/>
-        </FormItem>
+          <FormItem label="是否隐藏销量" name={['configs', 'hideSoldNum']} valuePropName="checked">
+            <Switch/>
+          </FormItem>
 
-        <FormItem label="库存计数" name="decStockMode">
-          <Radio.Group>
-            <Radio value={1}>付款减库存</Radio>
-            <Radio value={2}>拍下减库存</Radio>
-          </Radio.Group>
-        </FormItem>
+          <FormItem label="库存计数" name="decStockMode">
+            <Radio.Group>
+              <Radio value={1}>付款减库存</Radio>
+              <Radio value={2}>拍下减库存</Radio>
+            </Radio.Group>
+          </FormItem>
 
-        <FormItem label="是否可加入购物车" name="isAllowAddCart" valuePropName="checked" initialValue={true}>
-          <Switch/>
-        </FormItem>
+          <FormItem label="是否可加入购物车" name="isAllowAddCart" valuePropName="checked" initialValue={true}>
+            <Switch/>
+          </FormItem>
 
-        <FormItem label="是否可使用优惠券" name="isAllowCoupon" valuePropName="checked" initialValue={true}>
-          <Switch/>
-        </FormItem>
+          <FormItem label="是否可使用优惠券" name="isAllowCoupon" valuePropName="checked" initialValue={true}>
+            <Switch/>
+          </FormItem>
 
-        <FormItem label="支付时是否需填写地址" name="isRequireAddress" valuePropName="checked" initialValue={true}>
-          <Switch/>
-        </FormItem>
+          <FormItem label="支付时是否需填写地址" name="isRequireAddress" valuePropName="checked" initialValue={true}>
+            <Switch/>
+          </FormItem>
 
-        <FormItem label="支付时是否允许留言" name="isAllowComment" valuePropName="checked" initialValue={true}>
-          <Switch/>
-        </FormItem>
+          <FormItem label="支付时是否允许留言" name="isAllowComment" valuePropName="checked" initialValue={true}>
+            <Switch/>
+          </FormItem>
 
-        <FormItemSort/>
+          <FormItemSort/>
 
-        <FormItem name="id" type="hidden"/>
+          <FormItem name="id" type="hidden"/>
+        </Section>
 
         <FormAction/>
       </Form>
